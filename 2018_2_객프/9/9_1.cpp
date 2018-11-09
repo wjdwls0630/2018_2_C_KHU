71d08f933f555f0c71af6b798ae47484d32b887a
wjdwls0630 <wjdwls0630@gmail.com>
2018-11-09 16:11:49 +0900

    18.11.09

--- 2018_2_객프/9/9_1.cpp
+++ 2018_2_객프/9/9_1.cpp
@@ -15,7 +15,17 @@ int main() {
   pt2.x=-4;
   pt2.y=2.5;
 
-  std::cout << pt1.x <<pt1.y<< '\n';
+  std::cout <<"pt1 = ( "<< pt1.x <<", "<<pt1.y<<" )"<< '\n';
+  std::cout <<"pt2 = ( "<< pt2.x <<", "<<pt2.y<<" )"<< '\n';
+
+  pt1=pt2;
+  std::cout <<"pt1 = ( "<< pt1.x <<", "<<pt1.y<<" )"<< '\n';
+  std::cout <<"pt2 = ( "<< pt2.x <<", "<<pt2.y<<" )"<< '\n';
+
+  pt1.x=0;
+  std::cout <<"pt1 = ( "<< pt1.x <<", "<<pt1.y<<" )"<< '\n';
+  std::cout <<"pt2 = ( "<< pt2.x <<", "<<pt2.y<<" )"<< '\n';
+
 
 
   return 0;
